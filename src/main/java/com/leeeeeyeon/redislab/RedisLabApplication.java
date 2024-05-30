29ec3df0a0c065758376a4c16095b31e7e53c06f
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-05-30 23:41:37 +0900

    initial commit

---
+++ src/main/java/com/leeeeeyeon/redislab/RedisLabApplication.java
@@ -0,0 +1,13 @@
+package com.leeeeeyeon.redislab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLabApplication.class, args);
+    }
+
+}
